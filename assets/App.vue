45d6514b1bf5100493ae9df6439061b2f202aed5
Musashi <mushashiless@gmail.com>
2025-05-14 00:30:43 +0800

    Update App.vue

--- assets/App.vue
+++ assets/App.vue
@@ -615,7 +615,7 @@ export default {
           window.history.pushState(null, "", url.toString());
         }
         document.title = `${
-          this.cwd.replace(/.*\/(?!$)|\//g, "𝒐𝒇") ||
+          this.cwd.replace(/.*\/(?!$)|\//g, "") || ""
         } 𝑭𝒂𝒕𝒂𝒍𝒆𝒗𝒆𝒍`;
       },
       immediate: true,
